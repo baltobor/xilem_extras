f21511a93261c05d5fc067dc0575cff31d79b744
Jacek Wisniowski <jacek@baltobor.com>
2026-03-27 23:49:46 +0100

    tree_forest was added.

--- src/lib.rs
+++ src/lib.rs
@@ -103,10 +103,14 @@ pub use tree::{
     tree_group,
     tree_group_styled,
     tree_group_with_context_menu,
+    tree_forest,
+    tree_forest_styled,
+    tree_forest_with_context_menu,
     TreeAction,
     TreeStyle,
     ExpansionState,
     flatten_tree,
+    flatten_forest,
 };
 
 pub use list::{
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -9,4 +9,8 @@ mod expansion_state;
 mod tree_view;
 
 pub use expansion_state::ExpansionState;
-pub use tree_view::{tree, tree_group, tree_group_styled, tree_group_with_context_menu, TreeAction, TreeStyle, flatten_tree};
+pub use tree_view::{
+    tree, tree_group, tree_group_styled, tree_group_with_context_menu,
+    tree_forest, tree_forest_styled, tree_forest_with_context_menu,
+    TreeAction, TreeStyle, flatten_tree, flatten_forest,
+};
